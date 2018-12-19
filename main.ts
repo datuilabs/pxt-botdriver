f2539c1a964ff85964ee7c5ed79a5055eeb23c9c
datuilabs <39939895+datuilabs@users.noreply.github.com>
2018-12-19 18:40:20 +0800

    first block

--- main.ts
+++ main.ts
@@ -1,7 +1,28 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+enum DriveMode {
+    Programmic = 0,
+    Natural = 1,
+    PerWheel = 2,
+}
+
+//% color="#ff9800"
+namespace dtbd {
+
+    function createBuf(len: number, type: number) {
+        const buf = pins.createBuffer(len + 3);
+        buf.setNumber(NumberFormat.UInt8LE, 0, 42);
+        buf.setNumber(NumberFormat.UInt8LE, 1, len);
+        buf.setNumber(NumberFormat.UInt8LE, 2, type);
+        return buf;
+    }
+
+    const driveModeBuf = createBuf(1, 0);
+
+    //% block
+    export function setDriveMode(mode: DriveMode) {
+        driveModeBuf.setNumber(NumberFormat.UInt8LE, 3, mode);
+        serial.writeBuffer(driveModeBuf);
+
+        const resp = serial.readUntil("\r");
+        return resp == "ok";
+    }
+}
